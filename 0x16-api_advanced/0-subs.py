2835868737234bc29488d980123f20329a2e72c5
Nasef161995~ <mnasef905@gmail.com>
2024-02-13 23:14:37 +0300

    000

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
+"""function queries the Reddit API"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    "function queries the Reddit API and returns the number of subscribers"
+    """function queries the Reddit API and returns the number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     response = requests.get(url, allow_redirects=False)
