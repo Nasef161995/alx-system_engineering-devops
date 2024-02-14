c89ed0390f5d71395e2d2b9322f2881bf10c67cf
Nasef161995~ <mnasef905@gmail.com>
2024-02-14 11:30:22 +0300

    000

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """function queries the Reddit API"""
 import requests
-
+import re
 
 def number_of_subscribers(subreddit):
     """function queries the Reddit API and returns the number of subscribers"""
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "My Reddit API Client"}
+    url = f"https://www.reddit.com/r/{subreddit}/about/"
+    res = requests.get(url)
+    html = res.text
+    pattern = r'subscribers="([^"]*)"'
+    subscribers = re.search(pattern, html)
 
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
+    if subscribers:
+        return int(subscribers[0][13:-1])
     else:
         return 0
