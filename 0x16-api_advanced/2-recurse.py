3c1a418814e80740956931792f944d20c42af775
Nasef161995~ <mnasef905@gmail.com>
2024-02-14 11:35:54 +0300

    000

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,7 +7,7 @@ def recurse(subreddit, hot_list=[], after=None):
     """ returns a list containing the titles of all hot articles"""
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
-    res = requests.get(url, headers={'User-Agent': 'abeer'})
+    res = requests.get(url, headers={'User-Agent': 'mohamed'})
 
     if res.status_code != 200:
         return None
